70e445a422d2dafce1bbb9425c2502f656a0887f
Cucumbercon <seanpastore@gmail.com>
2023-09-24 02:01:57 -0400

    Added file structure and specified pages

---
+++ cinema-booking/pages/error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const error = () => {
+    return (
+        <div>
+            <h3>Error</h3>
+            <p>Page not available.</p>
+        </div>
+    )
+}
+
+export default error;
