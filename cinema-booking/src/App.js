1e8ddfe6d0a6c6ea01e70b94743014243124e9ec
coolestkvein <zxcasdqweze@gmail.com>
2023-09-27 20:44:15 -0400

    modified:   cinema-booking/src/App.js 	deleted:    cinema-booking/src/Homepage/searchBar.js 	new file:   cinema-booking/src/pages/Admin/adminIcon1.png 	new file:   cinema-booking/src/pages/Admin/adminIcon2.png 	new file:   cinema-booking/src/pages/Admin/adminIcon3.png 	new file:   cinema-booking/src/pages/Admin/adminIcon4.png 	new file:   cinema-booking/src/pages/Admin/adminIcon5.png 	new file:   cinema-booking/src/pages/Admin/adminPanel.css 	new file:   cinema-booking/src/pages/Admin/adminPanel.js 	deleted:    cinema-booking/src/pages/Admin/adminhome.js

--- cinema-booking/src/App.js
+++ cinema-booking/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 // import Navbar from './components/Navbar';
 import MovieBooking from './Homepage/home';
+import AdminPanel from './pages/Admin/adminPanel';
 
 function App() {
   return (
+    // <AdminPanel />
     <MovieBooking />
   );
   // return (
