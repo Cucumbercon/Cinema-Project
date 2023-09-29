18f1e6112f764301f7e3bacd787fd9d127e23a38
Lin <chenylbi1999@gmail.com>
2023-09-28 23:36:03 -0400

    Created a folder for spring controller

--- server/src/main/java/edu/uga/cinemabooking/controller/TheController.java
+++ server/src/main/java/edu/uga/cinemabooking/controller/TheController.java
@@ -1,4 +1,4 @@
-package edu.uga.cinemabooking;
+package edu.uga.cinemabooking.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class TheController {
 
     //*****************************retrieve message*********************************//
