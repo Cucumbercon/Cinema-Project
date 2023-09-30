ce386737bee89608c9d83ebf8baf6e9a9707e064
Lin <chenylbi1999@gmail.com>
2023-09-29 20:40:34 -0400

    added some controllers

---
+++ server/src/main/java/edu/uga/cinemabooking/controller/SigninController.java
@@ -0,0 +1,12 @@
+package edu.uga.cinemabooking.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class SigninController {
+    
+}
