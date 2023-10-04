a0aa62a03673cf1a5e1b75008952ed6d5e83d153
sn50802 <sn50802@uga.edu>
2023-10-03 20:22:16 -0400

    renamed:    server/src/main/java/edu/uga/cinemabooking/controller/TempTitleSearchController.java -> server/src/main/java/edu/uga/cinemabooking/controller/SearchMovieController.java modified:   server/src/main/java/edu/uga/cinemabooking/testCrypto.java

--- server/src/main/java/edu/uga/cinemabooking/controller/SearchMovieController.java
+++ server/src/main/java/edu/uga/cinemabooking/controller/SearchMovieController.java
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class TempTitleSearchController {
 
+public class SearchMovieController {
     MovieDB mdb = new MovieDB();
-    }
+
+    @PostMapping("/searchMovie");
+    
 }
