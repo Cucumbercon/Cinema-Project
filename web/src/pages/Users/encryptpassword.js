cc4e7308abf2158ca932044188ce84b09795265e
sn50802 <sn50802@uga.edu>
2023-10-06 09:27:48 -0400

    Temp file change

--- web/src/pages/Users/encryptpassword.js
+++ web/src/pages/Users/encryptpassword.js
@@ -1,1 +1,4 @@
 // temporary
+// Refer to this website https://stackoverflow.com/questions/57416217/cryptojs-encrypt-in-aes-256-cbc-returns-an-unexpected-value
+// Import crypto-js
+var CryptoJS = require("crypto-js");
