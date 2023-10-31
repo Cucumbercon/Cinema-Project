0b5dd040c6e005d8ec68fefd1379bc781e9e843e
sn50802 <sn50802@uga.edu>
2023-10-31 11:41:19 -0400

    modified:   web/src/pages/Users/login.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {toast,ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 // import Navbar from './components/Navbar';
 import { Login } from './pages/Users/login';
 import DashBoard from './pages/Admin/DashBoard';
@@ -17,7 +15,6 @@ import OrderSummary from './pages/OrderSummary/OrderSummary'
 import Checkout from './pages/Checkout/Checkout';
 import { Signup } from './pages/Users/signup';
 import { NotFound } from './pages/Users/NotFound';
-import { LoginFail } from './pages/Users/LoginFail';
 import { UpdateProfile } from './pages/Users/UpdateProfile';
 import EmailVerification from './pages/Users/EmailVerification';
 import RegistrationSuccessPage from './pages/Users/RegistrationSuccessPage';
@@ -28,12 +25,11 @@ function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [name, setName] = useState("");
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MovieBooking isLoggedIn={isLoggedIn} isAdmin={isAdmin} setIsLoggedIn={setIsLoggedIn} setIsAdmin={setIsAdmin} name={name}/>} />
+        <Route path="/" element={<MovieBooking isLoggedIn={isLoggedIn} isAdmin={isAdmin} setIsLoggedIn={setIsLoggedIn}/>} />
         <Route path="/dashboard" element={<DashBoard isAdmin={isAdmin}/>} />
         <Route path="/moviebooking" element={<MovieBooking />} />
         <Route path="/moviedetails" element={<MovieDetails />} />
@@ -44,28 +40,15 @@ function App() {
         <Route path="/ordersummary" element={<OrderSummary />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/confirmationpage" element={<ConfirmationPage />} />
-        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} setIsAdmin={setIsAdmin} setName={setName}/>} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} setIsAdmin={setIsAdmin}/>} />
         <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn}/>} />
         <Route path="/notfound" element={<NotFound />} />
-        <Route path="/loginFail" element={<LoginFail />} />
         <Route path="/updateprofile" element={<UpdateProfile/>} />
         <Route path="/emailverification" element={<EmailVerification/>} />
         <Route path="/registrationsuccess" element={<RegistrationSuccessPage/>} />
         <Route path="/forgotpass" element={<Forgotpass/>} />
 
       </Routes>
-      <ToastContainer
-              position="bottom-center"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="colored"
-            />
     </Router>
 
   );
--- web/src/pages/Users/login.js
+++ web/src/pages/Users/login.js
@@ -1,153 +1,124 @@
 import React, { useState } from "react";
 import { encrypt } from './encryption';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
-// import { toast, ToastContainer } from 'react-toastify';
+import {useNavigate}from 'react-router-dom';
 
 import './login2.css';
 
 export const Login = (props) => {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [plainPass, setPlainPass] = useState('');
-  const [rememberMe, setRememberMe] = useState(false);
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [plainPass, setPlainPass] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    const pass = encrypt(plainPass);
+        const pass = encrypt(plainPass);
 
-    const loginInfo = {
-      email, pass, rememberMe,
-    };
-    console.log(loginInfo)
-    fetch('http://localhost:8000/api/login', {
-      method: 'POST',
-      body: JSON.stringify(loginInfo),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(function (response) {
-      if (response.status === 200) {
-        response.json()
-          .then(function (jsonUser) {
-            props.setName(jsonUser.fullName);
-            props.setIsLoggedIn(true);
-            if (jsonUser.type === 1)
-              props.setIsAdmin(true);
-            return (
-              navigate("/")
-              );
+        const loginInfo = {
+          email, pass, rememberMe,
+        };
+        console.log(loginInfo)
+        fetch('http://localhost:8000/api/login', {
+        method: 'POST',
+        body: JSON.stringify(loginInfo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(function (response) {
+        if (response.status === 200) {
+          response.json()
+            .then(function (jsonUser) {
+              props.setIsLoggedIn(true);
+              if (jsonUser.type === 1)
+                props.setIsAdmin(true);
+              return navigate("/");
 
-            /**
-             * you can receive user's name, id, and type(0 is customer, 1 is admin):
-             * jsonUser.fullName, jsonUser.id, jsonUser.type
-             * 
-             * capture them here, if u need more user info, I can add them(e.g. address, phone number, email)
-             * 
-             * 
-             * direct to a welcome page or popup-window or something else to inform the user login successfully
-             * 
-             *  
-             * */
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-      } else if (response.status === 406) {
-        /**
-         * means Email and/or password do not match
-         * need to do something here to inform the user
-         * 
-         * 
-         * 
-         * */
+              /**
+               * you can receive user's name, id, and type(0 is customer, 1 is admin):
+               * jsonUser.fullName, jsonUser.id, jsonUser.type
+               * 
+               * capture them here, if u need more user info, I can add them(e.g. address, phone number, email)
+               * 
+               * 
+               * direct to a welcome page or popup-window or something else to inform the user login successfully
+               * 
+               *  
+               * */ 
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+        } else if (response.status === 406) {
+          /**
+           * means Email and/or password do not match
+           * need to do something here to inform the user
+           * 
+           * 
+           * 
+           * */ 
+          console.error('Request failed with status: ', response);
+        } else {
+          /**
+           * other errors
+           * maybe direct to error page, and put the response status into error page
+           * 
+           * 
+           *  */ 
 
-        console.error('Request failed with status: ', response.status);
-        toast.error("Incorrect username or password. Please try again.", {
-          position: "bottom-center",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-          });
+          console.error('Request failed with status: ', response.status);
+          return Promise.reject('request fail');
+        }
+      });
 
-      } else {
-        /**
-         * other errors
-         * maybe direct to error page, and put the response status into error page
-         * 
-         * 
-         *  */
+    }
 
-        console.error('Request failed with status: ', response.status);
-        return navigate("/loginFail");
-      }
-    });
+    return (
+        <div className="auth-form-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input
+                  value={email}
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  placeholder="youremail@gmail.com"
+                  id="email"
+                  name="email"
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                  value={plainPass}
+                  required
+                  onChange={(e) => setPlainPass(e.target.value)}
+                  type="password"
+                  placeholder="********"
+                  id="password"
+                  name="password"
+                />
+                <div className="login-remember-container">
+                  <button className="login-btn" type="login">Login</button>
+                  <div className="remember-me">
+                    <input
+                      type="checkbox"
+                      id="rememberMe"
+                      name="rememberMe"
+                      checked={rememberMe}
+                      onChange={(e) => setRememberMe(e.target.checked)}
+                    />
+                    <label htmlFor="rememberMe">Remember me</label>
+                  </div>
+                </div>
+            </form>
+            
 
-  }
-
-  return (
-    <div className="auth-form-container">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          value={email}
-          required
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="youremail@gmail.com"
-          id="email"
-          name="email"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          value={plainPass}
-          required
-          onChange={(e) => setPlainPass(e.target.value)}
-          type="password"
-          placeholder="********"
-          id="password"
-          name="password"
-        />
-        <div className="login-remember-container">
-          <button className="login-btn" type="login">Login</button>
-          <div className="remember-me">
-            <input
-              type="checkbox"
-              id="rememberMe"
-              name="rememberMe"
-              checked={rememberMe}
-              onChange={(e) => setRememberMe(e.target.checked)}
-            />
-            <label htmlFor="rememberMe">Remember me</label>
-          </div>
+            <button className="forgot-pass-btn" onClick={() => navigate('/forgotpass')}>
+              Forgot Password?
+            </button>
+            <button className="log-btn" onClick={() =>  navigate('/signup')}>
+              Don't have an account? Register here.
+            </button>
+            
         </div>
-      </form>
-
-
-      <button className="forgot-pass-btn" onClick={() => navigate('/forgotpass')}>
-        Forgot Password?
-      </button>
-      <button className="log-btn" onClick={() => navigate('/signup')}>
-        Don't have an account? Register here.
-      </button>
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-    </div>
-  )
+    )
 }
