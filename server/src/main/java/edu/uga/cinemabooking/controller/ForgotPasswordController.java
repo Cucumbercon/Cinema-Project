0642974857d81d4b80beb5ce23e54515d4457161
sn50802 <sn50802@uga.edu>
2023-10-30 22:22:01 -0400

    modified:   server/src/main/java/edu/uga/cinemabooking/controller/ForgotPasswordController.java

--- server/src/main/java/edu/uga/cinemabooking/controller/ForgotPasswordController.java
+++ server/src/main/java/edu/uga/cinemabooking/controller/ForgotPasswordController.java
@@ -24,57 +24,18 @@ public class ForgotPasswordController {
     int loggedID;
 
     /**
-     * This method is to validate the login
      * 
-     * @param data login email and password
-     * @return user
      */
     @PostMapping("/forgotpassword")
     public ResponseEntity<String> fetchData(@RequestBody String data) {
 
         ObjectMapper objectMapper = new ObjectMapper();
-        User user = null;
-
         try {
 
-            JsonNode jsonNode = objectMapper.readTree(data);
-            String email = jsonNode.get("email").asText();
-            String password = jsonNode.get("pass").asText();
-
-            if (udb.emailExist(email)) {
-                user = udb.loginValidation(email, password);
-                int userID = user.getId();
-                if (user != null) {
-                    String jsonUser = objectMapper.writeValueAsString(user);
-                    System.out.println("logged in");
-                    System.out.println(jsonUser);
-                    setID(userID);
-                    return ResponseEntity.ok(jsonUser);
-                } else {
-                    System.out.println("logged fail");
-                    return new ResponseEntity<>("Email and/or password do not match", HttpStatus.NOT_ACCEPTABLE);
-                }
-            } else {
-                // return 406 if email or password do not matc
-                System.out.println("logged fail");
-                return new ResponseEntity<>("Email and/or password do not match", HttpStatus.NOT_ACCEPTABLE);
-
-            }
-
-            // if something wrongs going on
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Invalid data format", HttpStatus.BAD_REQUEST);
-        }
+        } 
 
-    } // fetchData
-
-    public void setID(int id) {
-        loggedID = id;
-    } // setID
-
-    public int getID() {
-        return loggedID;
-    } // getID
+    }
 
 }
