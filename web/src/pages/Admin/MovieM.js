2037efaea5e1e51ae8bb406b718c8ee66762f1d9
Lin <chenylbi1999@gmail.com>
2023-11-05 16:07:06 -0500

    added "required" to all addMovie fields

--- web/src/pages/Admin/MovieM.js
+++ web/src/pages/Admin/MovieM.js
@@ -69,19 +69,19 @@ function MovieManagement(props) {
         <div className="movie-management">
             <h2>Movie Details</h2>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="title" placeholder="Movie Title" onChange={handleChange} />
-                <input type="text" name="language" placeholder="Language" onChange={handleChange} />
-                <input type="text" name="popularity" placeholder="Popularity" onChange={handleChange} />
-                <input type="text" name="poster_path" placeholder="Poster_path" onChange={handleChange} />
-                <input type="text" name="release_day" placeholder="Release_day" onChange={handleChange} />
+                <input type="text" name="title" placeholder="Movie Title" onChange={handleChange} required/>
+                <input type="text" name="language" placeholder="Language" onChange={handleChange} required/>
+                <input type="text" name="popularity" placeholder="Popularity" onChange={handleChange} required/>
+                <input type="text" name="poster_path" placeholder="Poster_path" onChange={handleChange} required/>
+                <input type="text" name="release_day" placeholder="Release_day" onChange={handleChange} required/>
                 <textarea name="synopsis" placeholder="Synopsis" onChange={handleChange}></textarea>
-                <input type="text" name="state" placeholder="State" onChange={handleChange} />
-                <input type="text" name="category" placeholder="Category" onChange={handleChange} />
-                <input type="text" name="rating" placeholder="Rating" onChange={handleChange} />
-                <input type="text" name="trailer_path" placeholder="Trailer_path" onChange={handleChange} />
-                <input type="text" name="cast" placeholder="Cast" onChange={handleChange} />
-                <input type="text" name="director" placeholder="Director" onChange={handleChange} />
-                <input type="text" name="producer" placeholder="Producer" onChange={handleChange} />
+                <input type="text" name="state" placeholder="State" onChange={handleChange} required/>
+                <input type="text" name="category" placeholder="Category" onChange={handleChange} required/>
+                <input type="text" name="rating" placeholder="Rating" onChange={handleChange} required/>
+                <input type="text" name="trailer_path" placeholder="Trailer_path" onChange={handleChange} required/>
+                <input type="text" name="cast" placeholder="Cast" onChange={handleChange} required/>
+                <input type="text" name="director" placeholder="Director" onChange={handleChange} required/>
+                <input type="text" name="producer" placeholder="Producer" onChange={handleChange} required/>
 
                 {/* <label>
                     Archived:
