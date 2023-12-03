c13c53e7ebcdc7c3e0f3840158d5f7660577a27a
sn50802 <sn50802@uga.edu>
2023-12-03 11:14:26 -0500

    Entity changes in backend

--- server/src/main/java/edu/uga/cinemabooking/entity/Showroom.java
+++ server/src/main/java/edu/uga/cinemabooking/entity/Showroom.java
@@ -1,5 +1,44 @@
 package edu.uga.cinemabooking.entity;
 
 public class Showroom {
-    
+    int id;
+    int seat_amount;
+    String information;
+    String name;
+
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for seat_amount
+    public int getSeatAmount() {
+        return seat_amount;
+    }
+
+    public void setSeatAmount(int seat_amount) {
+        this.seat_amount = seat_amount;
+    }
+
+    // Getter and Setter for information
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- server/src/main/java/edu/uga/cinemabooking/entity/Ticket.java
+++ server/src/main/java/edu/uga/cinemabooking/entity/Ticket.java
@@ -1,5 +1,47 @@
 package edu.uga.cinemabooking.entity;
 
 public class Ticket {
-    
+    int id;
+    int schedule_id;
+    private enum State {
+        available, reserved, occupied, expired
+    }
+    State state;
+    int seat_id;
+
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for schedule_id
+    public int getScheduleId() {
+        return schedule_id;
+    }
+
+    public void setScheduleId(int schedule_id) {
+        this.schedule_id = schedule_id;
+    }
+
+    // Getter and Setter for state
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    // Getter and Setter for seat_id
+    public int getSeatId() {
+        return seat_id;
+    }
+
+    public void setSeatId(int seat_id) {
+        this.seat_id = seat_id;
+    }
 }
