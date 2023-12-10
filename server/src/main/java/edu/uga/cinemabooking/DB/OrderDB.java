d8f4fda67478865361c403527d207d375dae4412
sn50802 <sn50802@uga.edu>
2023-12-10 10:07:22 -0500

    Add order implemented

--- server/src/main/java/edu/uga/cinemabooking/DB/OrderDB.java
+++ server/src/main/java/edu/uga/cinemabooking/DB/OrderDB.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import edu.uga.cinemabooking.entity.Schedule;
@@ -29,15 +30,30 @@ public class OrderDB {
         }
     }
 
-    public void addSeat(int ticket_id, int showroom_id, int column, int row) {
+    public void addOrder(int user_id, int payment_id, int ticket_id, int promote_id, int ticket_amount,
+    double total, String describe) {
 
-        String sql = "INSERT INTO seat (ticket_id, showroom_id, column, row) " +
-                "VALUES (?,?,?,?)";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String parsableString = sdf.format(Calendar.getInstance().getTime());
+        java.util.Date utilDate = null;
+        try {
+            utilDate = sdf.parse(parsableString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        java.sql.Timestamp sqlDate = new java.sql.Timestamp(utilDate.getTime());
+
+        String sql = "INSERT INTO order (user_id, payment_id, ticket_id, promote_id, ticket_amount, total, order_time, discribe) " +
+                "VALUES (?,?,?,?,?,?,?,?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1, ticket_id);
-            preparedStatement.setInt(2, showroom_id);
-            preparedStatement.setInt(3, column);
-            preparedStatement.setInt(4, row);
+            preparedStatement.setInt(1, user_id);
+            preparedStatement.setInt(2, payment_id);
+            preparedStatement.setInt(3, ticket_id);
+            preparedStatement.setInt(4, promote_id);
+            preparedStatement.setInt(5, ticket_amount);
+            preparedStatement.setDouble(6, total);
+            preparedStatement.setTimestamp(7, sqlDate);
+            preparedStatement.setString(8, describe);
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
