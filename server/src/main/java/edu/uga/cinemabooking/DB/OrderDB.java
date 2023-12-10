e4785da70614ba1da049134714df10afa15f06cb
sn50802 <sn50802@uga.edu>
2023-12-10 14:30:39 -0500

    addOrder fixed

--- server/src/main/java/edu/uga/cinemabooking/DB/OrderDB.java
+++ server/src/main/java/edu/uga/cinemabooking/DB/OrderDB.java
@@ -33,6 +33,23 @@ public class OrderDB {
     public void addOrder(int user_id, int payment_id, int ticket_id, int promote_id, int ticket_amount,
     double total, String describe) {
 
+        String sql = "INSERT INTO order (user_id, payment_id, ticket_id, promote_id, ticket_amount) " +
+                "VALUES (?,?,?,?,?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, user_id);
+            preparedStatement.setInt(2, payment_id);
+            preparedStatement.setInt(3, ticket_id);
+            preparedStatement.setInt(4, promote_id);
+            preparedStatement.setInt(5, ticket_amount);
+            preparedStatement.executeUpdate();
+            addOrderHelper(total, describe);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void addOrderHelper(double total, String describe) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String parsableString = sdf.format(Calendar.getInstance().getTime());
         java.util.Date utilDate = null;
@@ -42,23 +59,31 @@ public class OrderDB {
             e.printStackTrace();
         }
         java.sql.Timestamp sqlDate = new java.sql.Timestamp(utilDate.getTime());
-
-        String sql = "INSERT INTO order (user_id, payment_id, ticket_id, promote_id, ticket_amount, total, order_time, discribe) " +
-                "VALUES (?,?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO order (total, order_time, discribe) WHERE ID = " + findMaxId() + " VALUES (?,?,?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1, user_id);
-            preparedStatement.setInt(2, payment_id);
-            preparedStatement.setInt(3, ticket_id);
-            preparedStatement.setInt(4, promote_id);
-            preparedStatement.setInt(5, ticket_amount);
-            preparedStatement.setDouble(6, total);
-            preparedStatement.setTimestamp(7, sqlDate);
-            preparedStatement.setString(8, describe);
+            preparedStatement.setDouble(1, total);
+            preparedStatement.setTimestamp(2, sqlDate);
+            preparedStatement.setString(3, describe);
             preparedStatement.executeUpdate();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    public int findMaxId() {
+        String sql = "SELECT MAX(ID) as max_id FROM order";
+        int maxId = -1; // default value if no rows are found
+    
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+    
+            if (resultSet.next()) {
+                maxId = resultSet.getInt("max_id");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+        return maxId;
     }
 }
