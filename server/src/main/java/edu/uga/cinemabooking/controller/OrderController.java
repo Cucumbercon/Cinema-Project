6397647ea7d755a17362751368c389beae8f00a3
sn50802 <sn50802@uga.edu>
2023-12-11 10:43:30 -0500

    Temporary OrderController Finished

--- server/src/main/java/edu/uga/cinemabooking/controller/OrderController.java
+++ server/src/main/java/edu/uga/cinemabooking/controller/OrderController.java
@@ -1,5 +1,7 @@
 package edu.uga.cinemabooking.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.uga.cinemabooking.DB.OrderDB;
+import edu.uga.cinemabooking.DB.TicketDB;
 import edu.uga.cinemabooking.entity.Order;
 
 @RestController
@@ -20,6 +23,7 @@ public class OrderController {
 
     OrderDB odb = new OrderDB();
     Order order = new Order();
+    TicketDB tdb = new TicketDB();
 
     @PostMapping("/addOrder")
     public ResponseEntity<String> addOrder(@RequestBody String data) {
@@ -29,15 +33,21 @@ public class OrderController {
             JsonNode jsonNode = objectMapper.readTree(data);
             System.out.println(jsonNode);
 
-            
-            int id = jsonNode.get("orderData").get("user_id").asInt();
+
+            int user_id = jsonNode.get("orderData").get("user_id").asInt();
             double total = jsonNode.get("orderData").get("total").asDouble();
             int ticket_amount = jsonNode.get("orderData").get("ticket_amount").asInt();
+            int payment_id = jsonNode.get("orderData").get("payment_id").asInt();
+            String description = jsonNode.get("description").asText();
+            int ticket_id = 0;
 
 
             int schedule_id = jsonNode.get("schedule_id").asInt();
-
-            odb.addOrder(id, 0, 0, 0, ticket_amount, total, "");
+            List<Integer> ticket_ids = tdb.getTicketIDs(schedule_id);
+            for (int i = 0; i < ticket_ids.size(); i++) {
+                odb.addOrder(user_id, payment_id, ticket_ids.get(i), ticket_amount, total, description);
+            }
+            
 
         } catch (Exception e) {
             System.out.println("Error: " + e.toString());
